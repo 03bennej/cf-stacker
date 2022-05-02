6e3154d456f2e1edff4a048c535e981d7e00b321
03bennej <jjrbenn@gmail.com>
2022-05-02 15:50:54 -0400

    minor change

--- cf_stacker.py
+++ cf_stacker.py
@@ -132,10 +132,9 @@ class cf_stacker(BaseEstimator):
                                    alpha=self.alpha_nmf,
                                    update_H=True)
             np.random.seed(73584); W_init = np.random.rand(X.shape[0], self.latent_dimension)
-            np.random.seed(73584); H_init = np.random.rand(self.latent_dimension, X.shape[1])
             self.W_predict = self.nmf_predict.fit_transform(self.X_predict_masked,
                                                             W=W_init,
-                                                            H=H_init)
+                                                            H=self.H)
 
             if self.method == 'mean':
                 X_predict = np.mean(self.W_predict @ self.H, axis=1)

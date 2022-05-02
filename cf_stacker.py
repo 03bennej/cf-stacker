e27bc511ee581bf34a1e438491a645e912db89f5
03bennej <jjrbenn@gmail.com>
2022-05-02 17:28:19 -0400

    minor change

--- cf_stacker.py
+++ cf_stacker.py
@@ -144,7 +144,7 @@ class CFStacker(BaseEstimator):
             elif self.method == 'median':
                 X_predict = np.median(self.W_predict @ self.H, axis=1)
             elif self.method == 'lr':
-                X_predict = self.output_model.predict_proba(self.W_predict @ self.H)[1]
+                X_predict = self.output_model.predict_proba(self.W_predict @ self.H)[:, 1]
         else:
             if self.method == 'mean':
                 X_predict = np.nanmean(self.X_predict_masked, axis=1)

cb2776b059c29d5ceef5ce41dfa2913874741278
03bennej <jjrbenn@gmail.com>
2022-05-04 12:03:16 -0400

    minor change

--- cf_stacker.py
+++ cf_stacker.py
@@ -89,9 +89,9 @@ class CFStacker(BaseEstimator):
 
         #self.X_comb = X
 
-        unreliable_probs = np.abs(X - np.expand_dims(y, axis=1)) ** 1
-        unreliable_probs[unreliable_probs >= 0.5] = 1
-        unreliable_probs[unreliable_probs < 0.5] = 0
+        unreliable_probs = np.abs(X - np.expand_dims(y, axis=1))
+        unreliable_probs[unreliable_probs >= 0.5] = 1 # incorrect predictions
+        unreliable_probs[unreliable_probs < 0.5] = 0 # correct predictions
 
         self.basemodel.fit(X, unreliable_probs)
 

ba791ec39de049d98eefa6458a25188ecd6d005f
03bennej <jjrbenn@gmail.com>
2022-05-09 14:19:09 -0400

    minor change

--- cf_stacker.py
+++ cf_stacker.py
@@ -151,7 +151,7 @@ class CFStacker(BaseEstimator):
 
             # W_init = np.random.rand(self.X_comb_masked.shape[0], self.latent_dimension)
             W_init = np.concatenate((self.W_train,
-                                    np.random.rand(X.shape[0], self.latent_dimension)),
+                                    np.random.rand(self.X_predict_masked.shape[0], self.latent_dimension)),
                                     axis=0)
             print(np.shape(W_init))
             #print(np.shape(self.W_train))

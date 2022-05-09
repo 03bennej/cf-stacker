9f7b54c813551de46e80927e8f1eebe1aa7428ad
03bennej <jjrbenn@gmail.com>
2022-05-09 13:55:46 -0400

    minor change

--- cf_stacker.py
+++ cf_stacker.py
@@ -156,7 +156,7 @@ class CFStacker(BaseEstimator):
             self.W = self.nmf_predict.fit_transform(self.X_comb_masked,
                                                             W=W_init,
                                                             H=self.H)
-
+            self.H = self.nmf_predict.components_
             self.X_comb_reestimated = self.W @ self.H
 
             X_predict = self.X_comb_reestimated[self.X_train_shape[0]::, :]

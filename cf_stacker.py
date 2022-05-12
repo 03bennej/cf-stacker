365df0bff3b9669477ebf9d3a8cd09b93b0231eb
03bennej <jjrbenn@gmail.com>
2022-05-11 20:03:30 -0400

    minor change

--- cf_stacker.py
+++ cf_stacker.py
@@ -47,8 +47,8 @@ def remove_unreliable_entries(data,
     total_entries = len(np.ndarray.flatten(data_new))
     data_new[unreliable_entries > threshold] = target
     removed_entries = len(np.ndarray.flatten(data_new[np.isnan(data_new)]))
-    #print(f"Removed {removed_entries}/{total_entries} entries")
-    print(data_new)
+    print(f"Removed {removed_entries}/{total_entries} entries")
+    #print(data_new)
     return data_new
 
 

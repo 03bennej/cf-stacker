3c679ef859da5a7ef300dd0aac2df26d7472f473
03bennej <jjrbenn@gmail.com>
2022-05-23 11:13:19 -0400

    minor change

--- cf_stacker.py
+++ cf_stacker.py
@@ -66,7 +66,7 @@ def generate_mask(unreliable_probs,
 
 def apply_mask(data, mask, target=np.nan):
     data_new = np.copy(data)
-    data_new[mask] = target
+    data_new[mask is True] = target
     return data_new
 
 

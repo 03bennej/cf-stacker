153480c1b103cd2b30e2cd09280d3456d314bf61
03bennej <jjrbenn@gmail.com>
2022-05-24 11:00:55 -0400

    minor change

--- cf_stacker.py
+++ cf_stacker.py
@@ -47,6 +47,9 @@ def thresholds(X, y, beta=1):
 def generate_mask(unreliable_probs,
                   threshold=0.5):
     mask = unreliable_probs >= threshold
+    total_entries = len(np.flatten(mask))
+    total_unreliable_probs = len(np.flatten(mask[mask]))
+    print("Percentage removed", total_unreliable_probs/total_entries)
     return mask
 
 

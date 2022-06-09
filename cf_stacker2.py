d2eb45b769f24c9118566e9ee4af52109f1db1e5
03bennej <jjrbenn@gmail.com>
2022-06-09 17:12:39 -0400

    minor change

--- cf_stacker2.py
+++ cf_stacker2.py
@@ -33,10 +33,10 @@ def calculate_biases(X):
 
 
 def obj_fun(X_true, W, H, C, mu, b1, b2, lamW, lamH):
-    C[C >= 0.5] = 1
-    C[C < 0.5] = 0
+    # C[C >= 0.5] = 1
+    # C[C < 0.5] = 0
     X_pred = model(W, H, mu, b1, b2)
-    Cpow = tf.pow(tf.constant(C, dtype=tf.dtypes.float32), 1)
+    Cpow = tf.pow(tf.constant(C, dtype=tf.dtypes.float32), 2)
     Cpow = Cpow / tf.reduce_max(Cpow)
     wmse = tf.reduce_mean(tf.math.multiply(Cpow, tf.pow(X_true - X_pred, 2)))
     reg = lamW * tf.reduce_mean(tf.pow(W, 2)) + lamH * tf.reduce_mean(tf.pow(H, 2))

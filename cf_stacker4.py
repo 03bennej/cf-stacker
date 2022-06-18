18d2a072b3d29ced122d3ac0a8b184d857a24b32
03bennej <jjrbenn@gmail.com>
2022-06-18 11:35:56 -0400

    minor change

--- cf_stacker4.py
+++ cf_stacker4.py
@@ -177,7 +177,7 @@ class MatrixFactorizationClassifier(BaseEstimator):
         
         self.y_predict = np.median(X, axis=1) #self.lr_model.predict(X)
         
-        self.C_predict = 4*(X-0.5)**2 # 1 - np.abs(X - np.expand_dims(self.y_predict, axis=1))
+        self.C_predict = 16*(X-0.5)**4 # 1 - np.abs(X - np.expand_dims(self.y_predict, axis=1))
         
         self.X_shape = np.shape(X)
 

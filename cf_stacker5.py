f4023a210f7409f07a3d8926dd7458be2c19e8fc
03bennej <jjrbenn@gmail.com>
2022-07-08 13:09:34 -0400

    minor change

--- cf_stacker5.py
+++ cf_stacker5.py
@@ -224,7 +224,7 @@ class MatrixFactorizationClassifier(BaseEstimator):
         with tf.GradientTape() as tape:
             self.Xh_test = model(self.W_test, self.H, self.mu_train, self.bw_test, self.bh_train)
             self.yh_test = logistic_regression(self.Xh_test, self.omega, self.beta)
-            self.C_test = calc_C(Xh_test, self.yh_test)
+            self.C_test = calc_C(X_test, self.yh_test)
             mf_loss = self.test_loss(X_train, self.Xh_train, self.yh_train, self.W_train, self.H, self.C_train) \
                       + self.test_loss(X_test, self.Xh_test, self.yh_test, self.W_test, self.H, self.C_test)
 
@@ -260,7 +260,7 @@ class MatrixFactorizationClassifier(BaseEstimator):
         step = 0
         self.Xh_test = model(self.W_test, self.H, self.mu_train, self.bw_test, self.bh_train)
         self.yh_test = format_lr(logistic_regression(self.X_test, self.omega, self.beta))  # should be Xh
-        self.C_test = calc_C(self.Xh_test, self.yh_test)
+        self.C_test = calc_C(X_test, self.yh_test)
         mf_loss = self.test_loss(X_train, self.Xh_train, self.yh_train, self.W_train, self.H, self.C_train) \
                   + self.test_loss(X_test, self.Xh_test, self.yh_test, self.W_test, self.H, self.C_test)
 

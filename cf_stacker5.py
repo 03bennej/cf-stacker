e46935606fa539502bd53d90c83b9156914eba74
03bennej <jjrbenn@gmail.com>
2022-07-11 10:21:12 -0400

    minor change

--- cf_stacker5.py
+++ cf_stacker5.py
@@ -105,7 +105,7 @@ def define_variables(X_shape, latent_dim):
 
 def calc_C(X, y, numpy=False):  # return binary matrix
     # C = 1 - tf.math.abs(X - y)
-    C = tf.math.floor(1 - tf.math.abs(X - y) + 1 / 4)
+    C = tf.math.floor(1 - tf.math.abs(X - y) + 1 / 2)
     if numpy:
         C = C.numpy()
     return C
